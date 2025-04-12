a289e838692fb9e24291d69fa883741148552707
Carlos Andres Orozco Illera <carlosandresorozcoillera@gmail.com>
2025-04-11 19:50:51 -0500

    Commune Edit

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -45,6 +45,8 @@
       <td>{{ $comuna->muni_nomb }}</td>
       <td>
       
+      <a href="{{ route('comunas.edit', ['comuna'=>$comuna->comu_codi]) }}" class="btn btn-info">Editar</a>
+
       <form action=" {{ route('comunas.destroy', ['comuna' => $comuna->comu_codi]) }}"
       method='POST' style="display: inline-block">
       @method('delete')
