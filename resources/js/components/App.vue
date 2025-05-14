e180173e5701ec9a162965019af1e57be184b5ad
carlos3571 <carlosandresorozcoillera@gmail.com>
2025-05-14 16:37:27 -0500

    vue.js

---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center mt-5">
+    <h1 class="text-success">Hola desde Vue 3 + Laravel + main.js </h1>
+    <font-awesome-icon icon="fa-solid fa-phone" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+}
+</style>
--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,22 @@ use App\Http\Controllers\api\MunicipioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas');
+//Route::get('/user', function (Request $request) {
+    //return $request->user();
+//})->middleware('auth:sanctum');
+//Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas');
+
+Route::middleware('auth: sanctum')->get('/user', function (Request $request) {
+return $request->user();
+});
 
 
 
 Route::apiResource('comunas', ComunaController::class);
+Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
+Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas');
+Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
+Route::get('/comunas/{comuna}', [ComunaController::class, 'show'])->name('comunas.show');
+Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
+
+Route::get('/municipios', [MunicipioController::class, 'index'])->name('municipios');
