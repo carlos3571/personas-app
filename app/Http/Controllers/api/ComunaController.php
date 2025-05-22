b3c09513a264bc479149f519a3d48a0e7fdf1078
carlos3571 <carlosandresorozcoillera@gmail.com>
2025-05-21 20:58:03 -0500

    destroy($id)  update(Request $request, $id)

--- app/Http/Controllers/api/ComunaController.php
+++ app/Http/Controllers/api/ComunaController.php
@@ -20,32 +20,60 @@ class ComunaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
-    }
+     
+     
+ 
+public function store(Request $request)
+{
+    $comuna = new Comuna();
+    $comuna->comu_nomb = $request->comu_nomb;
+    $comuna->muni_codi = $request->muni_codi;
+    $comuna->save();
 
+    return json_encode(['comuna' => $comuna]);
+}
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
+   public function show($id)
+{
+    $comuna = Comuna::find($id);
+    $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+
+    return json_encode(['comuna' => $comuna, 'municipios' => $municipios]);
+}
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+    public function update(Request $request, $id)
+{
+    $comuna = Comuna::find($id);
+    $comuna->comu_nomb = $request->comu_nomb;
+    $comuna->muni_codi = $request->muni_codi;
+    $comuna->save();
+
+    return json_encode(['comuna' => $comuna]);
+}
+
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
-    }
+    public function destroy($id)
+{
+    $comuna = Comuna::find($id);
+    $comuna->delete();
+
+    $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+        ->get();
+
+    return json_encode(['comunas' => $comunas, 'success' => true]);
+}
+
 }
